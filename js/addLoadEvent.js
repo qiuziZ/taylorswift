36bd01974eb31d3aa3680dbdccc47f68ce083e0e
qiuziZ <qiuzhi.zhu@shuyun.com>
2016-08-10 14:55:39 +0800

    taylorswift

---
+++ js/addLoadEvent.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Anly.Z on 16/7/26.
+ */
+
+//add onload func
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function () {
+			oldonload();
+			func();
+		}
+	}
+}
